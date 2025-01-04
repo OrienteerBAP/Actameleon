df4897825244bbfa22ed620facb2061893e616e7
Ilia Naryzhny <phantom@ydn.ru>
2025-01-04 15:05:41 -0800

    Update SceneDisplay to show placeholder for inactive lines

--- src/components/SceneDisplay.vue
+++ src/components/SceneDisplay.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="p-4 border rounded-lg mb-4 sm:p-6 md:p-8 lg:p-10 xl:p-12">
     <h2 class="text-2xl font-semibold mb-2 sm:text-3xl">Scene {{ scene.sceneNumber }}<span v-if="scene.title">: {{ scene.title }}</span></h2>
-    <div v-for="line in scene.lines" :key="line.id" class="mb-2 sm:mb-3 md:mb-4 lg:mb-5 xl:mb-6" v-show="line.active">
-      <LineDisplay :line="line" />
+    <div v-for="line in scene.lines" :key="line.id" class="inline text-left">
+      <LineDisplay :line="line"  v-if="line.active"/>
+      <span v-else class="text-gray-400">*</span>
     </div>
   </div>
 </template>
